69580d5d12d3e3e885d7b30d3c1a363ac4a519cc
ilyasnafal <ilyasnafal041@gmail.com>
2026-07-22 17:53:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ else:
         st.rerun()
         
     st.sidebar.markdown("---")
-    st.sidebar.title("📂 Form Inpu Data")
+    st.sidebar.title("📂 Form Input Data")
     
     if st.session_state['last_uploaded']:
         st.sidebar.info(f"💾 **Memori Aktif:**\n{st.session_state['last_uploaded']}")
